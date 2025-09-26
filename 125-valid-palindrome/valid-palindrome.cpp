0b4c3f0e708d62e903a02a2a54520a9cfbf64233
mrabh1jeet <166050860+mrabh1jeet@users.noreply.github.com>
2025-09-27 01:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (78.88%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,14 @@
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0, right = s.size() - 1;
         while (left < right) {
-            // Skip non-alphanumeric characters
             while (left < right && !isalnum(s[left])) left++;
             while (left < right && !isalnum(s[right])) right--;
-
-            // Compare lowercase versions
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            }
-
+            if (tolower(s[left]) != tolower(s[right])) return false;
             left++;
             right--;
         }
